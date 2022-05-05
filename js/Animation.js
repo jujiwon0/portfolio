660f93e30c5c5f34a7fd7ccefaf4fb218921b5f6
simseonbeom <seonbeom2@gmail.com>
2022-05-05 21:56:43 +0900

    가로 스크롤 구현

--- js/Animation.js
+++ js/Animation.js
@@ -2,43 +2,51 @@
 
 export function Animation (){
 
-    console.log('hello');
-/*
+    // console.log('hello');
+
+    gsap.to('#section03 .horizontal', { // 누구를 ?
+        // rotation: 360,
+        x: -innerWidth*3, // 어떻게 움직일 건지?
+        ease:'none', // 가속도 없음
+        scrollTrigger: {
+            trigger: "#section03", // 누구를 기준으로 ?
+            start: "top top", // 어디서부터 시작할건지?
+            end: () => "+=" + document.querySelector('#section03 .horizontal').offsetWidth, // 어디까지
+            // end: 'bottom bottom',
+            pin: true,
+            // pinSpace:false,
+            // pinSpacing: false,
+            scrub: true,
+            // markers: true
+        }
+    });
 
 
-        gsap.to('.fix_section', {
-            rotation: 360,
-            // x: -innerWidth*3,
-            scrollTrigger: {
+
+
+
+
+    /*
+
+
+
+
+            let sc02 = gsap.timeline();
+            ScrollTrigger.create({
+                animation: sc02,
                 trigger: "#section02",
                 start: "top top",
-                // end: () => "+=" + 300,
-                end: 'bottom bottom',
-                pin: true,
-                // pinSpace:false,
+                end: "bottom bottom",
+                pin:true,
                 pinSpacing: false,
-                scrub: true,
-                markers: true
-            }
-        });
+                scrub: 1,
+            });
+
+            sc02.to(".box",{x:100},0);
+            sc02.to(".box2",{y:100},1);
 
 
-        let sc02 = gsap.timeline();
-        ScrollTrigger.create({
-            animation: sc02,
-            trigger: "#section02",
-            start: "top top",
-            end: "bottom bottom",
-            pin:true,
-            pinSpacing: false,
-            scrub: 1,
-        });
-
-        sc02.to(".box",{x:100},0);
-        sc02.to(".box2",{y:100},1);
-
-
-*/
+    */
 
 
 
